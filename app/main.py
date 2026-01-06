16fa771af0dfb2ee211f8e2f2aab8e466b9e6621
TiagoMadeira <tiagopmadeira@gmail.com>
2026-01-06 20:12:24 +0000

    Fix: run app with uvicorn

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from .routers import calculator
+from app.routers import calculator
+import uvicorn
 
 app = FastAPI()
 app.include_router(calculator.router)
@@ -8,4 +9,8 @@ app.include_router(calculator.router)
 @app.get('/heatlhz')
 def healthz():
     return JSONResponse( code = 200 )
+
+if __name__ == '__main__':
+    print("Webserver: rest service starting")
+    uvicorn.run( app, host='0.0.0.0', port=5000)
 
