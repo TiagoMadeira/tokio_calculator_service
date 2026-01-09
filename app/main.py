23e6f80e44da41f69971263ecba5586cc3b2f554
TiagoMadeira <tiagopmadeira@gmail.com>
2026-01-09 15:12:04 +0000

    feature: Add opentelemetry data collection

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from app.routers import calculator
 import uvicorn
 
+# --------------------------
+# OpenTelemetry Tracing
+# --------------------------
+def otel_trace_init():
+    trace.set_tracer_provider(
+       TracerProvider(
+           resource=Resource.create({}),
+       ),
+    )
+
+otel_trace_init()
 app = FastAPI()
+FastAPIInstrumentor().instrument_app(app)
 app.include_router(calculator.router)
 
 @app.get('/heatlhz')
